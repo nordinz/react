2940c04467b8d72cf9484eeaaa4b9c78b498ac45
nordinz <nordin.zerhouni@gmail.com>
2023-06-06 15:05:47 +0200

    Initial commit todo

---
+++ react/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './styles.css';
+import { NewTodoForm } from './NewTodoForm';
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm />
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.length === 0 && 'No Todos'}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  value="Three"
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
